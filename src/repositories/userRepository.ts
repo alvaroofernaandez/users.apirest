4bcaff37addfd20106d22ea8cf8d55d8f7da4a46
alvaroofernaandez <alvaroofernaandez@gmail.com>
2025-01-28 12:16:39 +0100

    Agregar modelo de usuario y métodos CRUD en UserRepository y UserService

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,15 +1,27 @@
+import { UserModel } from "@models/Users";
 import { IUserRepository, User } from "types/UserTypes";
 
 export class UserRepository implements IUserRepository{
   
-  private users: User[] = [];
-
   async create(data:User): Promise<User>{
-    this.users.push(data);
-    return data;
+    const newUser = new UserModel(data);
+    return await newUser.save();
   }
 
   async find(): Promise<User[]>{
-    return this.users;
+    return await UserModel.find().exec();
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return await UserModel.findById(id).exec();
+  }
+
+  async update(id: string, data: User): Promise<User | null> {
+    return await UserModel.findByIdAndUpdate(id, data, {new: true}).exec();
+  }
+
+  async delete(id: string): Promise<boolean> {
+    const deleted = await UserModel.findByIdAndDelete(id).exec();
+    return deleted !== null;
   }
 }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -14,16 +14,35 @@ export default () => {
     res.send("La api está funcionando con éxito");
   });
 
+  //METODOS GET
   router.get("/users", async (req, res) => {
     const users = await userService.findUsers();
     res.json(users);
   });
 
+  router.get("/users/:id", async (req, res) => {
+    const users = await userService.findUsersById(req.params.id);
+    res.json(users);
+  });
+
+  //METODOS POST
   router.post("/users", async (req, res) => {
     const newUser = req.body;
     const result = await userService.createUser(newUser);
 
     res.json(result);
+  });
+
+  //METODOS PUT
+  router.put("/users/:id", async (req, res) => {
+    const users = await userService.updateUser(req.params.id, req.body);
+    res.json(users);
+  });
+
+  //METODOS DELETE
+  router.delete("/users/:id", async (req, res) => {
+    const users = await userService.deleteUser(req.params.id);
+    res.json(users);
   });
 
   return router;
