56ccfcd0b0e9b1151a0f02ee4c546e35df2caf64
alvaroofernaandez <alvaroofernaandez@gmail.com>
2025-01-28 11:12:06 +0100

    Implementación de UserRepository y UserService con rutas para crear y obtener usuarios

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,30 @@
+import { UserRepository } from "@repositories/userRepository";
+import { UserService } from "@services/userService";
 import { Router } from "express";
+import { IUserRepository, IUserService } from "types/UserTypes";
 
 const router = Router();
+
+const userRepository: IUserRepository = new UserRepository();
+const userService: IUserService = new UserService(userRepository);
+
 
 export default () => {
   router.get("/health", (req, res) => {
     res.send("La api está funcionando con éxito");
-  })
+  });
+
+  router.get("/users", async (req, res) => {
+    const users = await userService.findUsers();
+    res.json(users);
+  });
+
+  router.post("/users", async (req, res) => {
+    const newUser = req.body;
+    const result = await userService.createUser(newUser);
+
+    res.json(result);
+  });
 
   return router;
 };
